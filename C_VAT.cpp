0c987f2a03d9401effb37595bfda7b500c67ed91
GufuSong <ly2148103921@163.com>
2026-03-21 11:39:21 +0800

    first submit

---
+++ C_VAT.cpp
@@ -0,0 +1,132 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "C_VAT.h"
+#include "Engine/Engine.h"
+#include "NiagaraComponent.h"
+#include "NiagaraActor.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Materials/MaterialInstanceConstant.h"
+#include "Engine/StaticMesh.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SceneComponent.h"
+
+// Sets default values
+AC_VAT::AC_VAT()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	//Debug
+	UE_LOG(LogTemp, Log, TEXT("Create C_VAT"));
+
+	//初始化Mesh
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	SetRootComponent(MeshComponent);
+	
+	///Script/Engine.StaticMesh'/Game/SongGuFu_VFXContent/Ability/ID11_TencentTest/Art/VAT/VAT_Ground_5/vertex_animation_textures1_mesh.vertex_animation_textures1_mesh'
+	ConstructorHelpers::FObjectFinder<UStaticMesh> InMesh(
+		TEXT("/Game/SongGuFu_VFXContent/Ability/ID11_TencentTest/Art/VAT/VAT_Ground_5/vertex_animation_textures1_mesh.vertex_animation_textures1_mesh")
+	);
+
+	MeshComponent->SetStaticMesh(InMesh.Object);
+	
+	
+	//初始化Material
+	ConstructorHelpers::FObjectFinder<UMaterialInstanceConstant> InMaterial(
+		TEXT("/Game/SongGuFu_VFXContent/Ability/ID11_TencentTest/Material/Niagara/MaterialSource/M_AID11_VAT_Inst.M_AID11_VAT_Inst")
+	);
+
+	//初始化材质
+	AllMaterial.SetNum(1);
+	AllMaterial[0] = FStMaterialSlot(0, InMaterial.Object);
+
+	
+}
+
+
+
+// Called when the game starts or when spawned
+void AC_VAT::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//初始化Mesh
+	
+	if (InVATMesh)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Replacement Mesh"));
+		MeshComponent->SetStaticMesh(InVATMesh);
+	}
+	//初始化MeshTransform
+	InitialPosition = GetActorLocation() + OffsetPosition;
+	MeshComponent->SetWorldLocation(InitialPosition);
+	
+	MeshComponent->SetRelativeScale3D(Scale);
+
+
+
+
+	//将材质添加至Mesh,并初始化材质实例
+	for (FStMaterialSlot& Slot : AllMaterial)
+	{
+		if (!Slot.Material) continue;
+
+		UMaterialInstanceDynamic* DynMat =
+			UMaterialInstanceDynamic::Create(Slot.Material, this);
+
+		if (!DynMat) continue;
+		DynamicMaterials.Add(DynMat);
+
+		if (MeshComponent)
+		{
+			MeshComponent->SetMaterial(Slot.Index, DynMat);
+		}
+	}
+
+	//注册Niagara
+	ANiagaraActor* OwnerNiagara = Cast<ANiagaraActor>(GetOwner());
+
+	if (OwnerNiagara)
+	{
+		//UNiagaraComponent* NiagaraComp = OwnerNiagara->GetNiagaraComponent();
+
+		OwnerNiagara->GetNiagaraComponent()->SetVariableObject(TEXT("User.CallBackObject"), this);
+
+	}
+
+
+}
+
+// Called every frame
+void AC_VAT::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+//核心逻辑实现
+void AC_VAT::ReceiveParticleData_Implementation(const TArray<FBasicParticleData>& Data, UNiagaraSystem* NiagaraSystem, const FVector& SimulationPositionOffset)
+{
+	if (Data.Num() == 0) return;
+
+	if (Data[0].Velocity[0] < 1) { Destroy(); }
+
+	MeshComponent->SetWorldLocation(InitialPosition + Data[0].Position);
+	MeshComponent->SetRelativeScale3D(Scale* FVector(1,1,Data[0].Velocity[1]));
+
+	for (UMaterialInstanceDynamic* Mat : DynamicMaterials)
+	{
+		if (!Mat) continue;
+		/*
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			5.0f,
+			FColor::Yellow,
+			FString::Printf(TEXT("Size: %f"), Data[0].Size)
+		);
+		*/
+		Mat->SetScalarParameterValue(TEXT("Time"), Data[0].Size);
+	}
+	
+}
---
+++ C_VAT.h
@@ -0,0 +1,97 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NiagaraDataInterfaceExport.h"
+#include "C_VAT.generated.h"
+
+
+class UMaterialInstanceConstant;
+class UStaticMeshComponent;
+class INiagaraParticleCallbackHandler;
+
+
+USTRUCT(BlueprintType)
+struct FStMaterialSlot
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Index;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UMaterialInstanceConstant* Material;
+
+
+
+	FStMaterialSlot()
+		: Index(0)
+		, Material(nullptr)
+	{ }
+
+	FStMaterialSlot(int32 InIndex, UMaterialInstanceConstant* InMat)
+		: Index(InIndex)
+		, Material(InMat)
+	{ }
+};
+
+
+
+
+UCLASS()
+class SONGGUFU_VFX_V2_API AC_VAT : public AActor, public INiagaraParticleCallbackHandler
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AC_VAT();
+
+	// 声明（BlueprintNativeEvent）
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void ReceiveParticleData(
+		const TArray<FBasicParticleData>& Data,
+		UNiagaraSystem* NiagaraSystem,
+		const FVector& SimulationPositionOffset);
+
+	// C++实现
+	virtual void ReceiveParticleData_Implementation(
+		const TArray<FBasicParticleData>& Data,
+		UNiagaraSystem* NiagaraSystem,
+		const FVector& SimulationPositionOffset) override;
+
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FStMaterialSlot> AllMaterial;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMesh* InVATMesh;
+
+	UPROPERTY(EditAnywhere)
+	FVector OffsetPosition = FVector(0, 0, -100);
+
+	UPROPERTY(EditAnywhere)
+	FVector Scale = FVector(1, 1, 1);
+
+
+private:
+	//动态材质
+	TArray<UMaterialInstanceDynamic*> DynamicMaterials;
+
+	FVector InitialPosition;
+
+	UStaticMeshComponent* MeshComponent;
+};
